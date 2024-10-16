332197193f5b3a0be7fa1a87c52aa5fcde4db7b1
Igor de Castro <igor.cbs@gmail.com>
2024-10-16 14:55:49 -0300

    Remove SceneDelegate e Storyboard

--- Project 1/ViewController.swift
+++ Project 1/ViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    private let service: ServiceProtocol?
+final class ViewController: UIViewController {
+    private let service: ServiceProtocol
 
     init(service: ServiceProtocol) {
         self.service = service
@@ -20,22 +20,30 @@ class ViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .blue
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .blue
-        service?.getData(url: "https://graphql.anilist.co") { result in
-            switch result {
-            case .success(let success):
-//                do {
-                print(success)
-//                } catch {
-//                    print("Erro no parce")
-//                }
-            case .failure(let failure):
-                print("Deu ruim: \(failure.localizedDescription)")
-            }
-        }
+        print(#function)
     }
+    
+//    func configService() {
+//        service?.getData(url: "https://graphql.anilist.co") { result in
+//            switch result {
+//            case .success(let success):
+////                do {
+//                print(success)
+////                } catch {
+////                    print("Erro no parce")
+////                }
+//            case .failure(let failure):
+//                print("Deu ruim: \(failure.localizedDescription)")
+//            }
+//        }
+//    }
 }
 
